9639fc9eb2874656ea2c07fbc6c4d89d8d5e0de0
souravq <souravbera515@gmail.com>
2022-02-21 23:19:07 +0530

    Assignment 1 Done !

--- assignments/assignment_1/index.js
+++ assignments/assignment_1/index.js
@@ -1,14 +1,33 @@
+//require('dotenv').config();
 function getNameFromCommandLine() {
     // Write you code here, name should be taken as args in process.argv
+    //var arguments = process.argv
+    //const firstName = process.argv[2];
+    return process.argv[2];
 }
 
 function getNameFromEnv() {
     // Write your code here
+    return process.env.USERNAME;
 }
 
 function getNameFromReadLine() {
     // Write your code here
+    const readline = require("readline");
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    rl.question("Please enter your name", (ans) =>{
+        console.log(ans);
+        //return ans;
+        rl.close();
+    })
 }
+console.log(getNameFromCommandLine());
+console.log(getNameFromEnv());
+getNameFromReadLine();
 
 module.exports = {
     getNameFromCommandLine,
